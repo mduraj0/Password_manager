3e0b927e811120d86ff02e6cca210a3ed1ccfed5
Michał <durajczykmichal@gmail.com>
2022-12-07 13:56:25 +0100

    Initial commit

--- install.py
+++ install.py
@@ -1,3 +1,10 @@
+from sqlalchemy import create_engine, MetaData, Table, Column
+
 
 def install():
-    pass
+    engine = create_engine('sqlite:///database.db', future=True)
+    meta = MetaData()
+    portals = Table(
+        'portals', meta
+        Column()
+    )
