f6ea09420bffca545c5c942325b8a2cfa98ba82d
Michał <durajczykmichal@gmail.com>
2022-12-07 11:54:16 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import tkinter as tk
-
+from tkinter import ttk
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Password Manager')
+
+    tabs = ttk.Notebook(root)
+
+    cred_tab = tk.Frame(tabs)
+    add_cred_tab = tk.Frame(tabs)
+    tabs.add(cred_tab, text='Credentials')
+    tabs.add(add_cred_tab, text='Add password')
+
+    tabs.pack(expand=1, fill='both')
+
+    tree = ttk.Treeview()
+
     root.mainloop()
