f9e827d702939e15d323122fc79694b86a1f0dc8
Michał <durajczykmichal@gmail.com>
2022-12-07 13:59:20 +0100

    Initial commit

--- install.py
+++ install.py
@@ -5,7 +5,17 @@ def install():
     engine = create_engine('sqlite:///database.db', future=True)
     meta = MetaData()
     portals = Table(
-        'portals', meta
+        'portals', meta,
         Column('id', Integer, primary_key=True),
         Column('name', String)
     )
+
+    credentials = Table(
+        'credentials', meta,
+        Column('id', Integer, primary_key=True),
+        Column('portal_id', Integer),
+        Column('login', String),
+        Column('password', String)
+    )
+
+    meta.create_all(engine)
