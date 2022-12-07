9c2d0b6f50fcaa1e804da3ef684366f5ae234225
Michał <durajczykmichal@gmail.com>
2022-12-07 12:09:39 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
+from dataclasses import dataclass
+
+
+@dataclass
+class DTOCredentials:
+    portal: str
+    login: str
+
 
 if __name__ == '__main__':
+    credentials = [
+        DTOCredentials('o2.pl', 'duraj9@wp.pl'),
+        DTOCredentials('facebook.pl', 'duraj4@o2.pl'),
+        DTOCredentials('gmail.com', 'durajczykmichal@gmail.com')
+    ]
+
     root = tk.Tk()
     root.title('Password Manager')
 
@@ -14,6 +28,30 @@ if __name__ == '__main__':
 
     tabs.pack(expand=1, fill='both')
 
-    tree = ttk.Treeview()
+    tree = ttk.Treeview(
+        cred_tab,
+        columns=('portal', 'login'),
+        show='headings',
+        height=10
+    )
+    tree.column(
+        '#1',
+        anchor=tk.CENTER,
+        stretch=tk.NO,
+        width=200
+    )
+    tree.heading('#1', text='portal')
+    tree.column(
+        '#2',
+        anchor=tk.CENTER,
+        stretch=tk.NO,
+        width=200
+    )
+    tree.heading('login', text='login')
+
+    for credential in credentials:
+        tree.insert('', 'end', values=(credential.portal, credential.login))
+
+    tree.pack()
 
     root.mainloop()
