3fe725d776047428bb4f0f4fa412aab7abede95b
Michał <durajczykmichal@gmail.com>
2022-12-08 12:37:52 +0100

    Initial commit

--- crypto.py
+++ crypto.py
@@ -4,4 +4,11 @@ from base64 import b64encode
 
 
 class Crypto:
-    def __init__(self):
+    def __init__(self, pin):
+        self.pin = Crypto.prepare_pin(pin)
+        self.fernet = Fernet(self.pin)
+
+    @staticmethod
+    def prepare_pin(pin):
+
+        
