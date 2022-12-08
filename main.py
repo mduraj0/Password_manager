9545f36dcff01b0500435a4efbe89cb50f874046
Michał <durajczykmichal@gmail.com>
2022-12-08 13:37:51 +0100

    Initial commit

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from install import install
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from models import Credentials, Portal
-
+from crypto import Crypto
+from cryptography.fernet import InvalidToken
 
 @dataclass
 class DTOCredentials:
@@ -15,10 +16,11 @@ class DTOCredentials:
 
 
 class AddPassword:
-    def __init__(self, tab, db, credential_list, tabs):
+    def __init__(self, tab, db, credential_list, tabs, password):
         self.db = db
         self.credential_list = credential_list
         self.tabs = tabs
+        self.crypto = Crypto(password)
 
         button = ttk.Button(tab, text='Add password')
         portal_label = ttk.Label(tab, text='Portal:')
@@ -28,7 +30,7 @@ class AddPassword:
         self.login = ttk.Entry(tab)
 
         password_label = ttk.Label(tab, text='Password:')
-        self.password = ttk.Entry(tab)
+        self.password = ttk.Entry(tab, show='*')
 
         portal_label.grid(row=0, column=0, padx=5)
         self.portal.grid(row=0, column=1, pady=5)
@@ -48,7 +50,7 @@ class AddPassword:
             portal = Portal(name=self.portal.get())
             cred = Credentials(
                 login=self.login.get(),
-                password=self.password.get(),
+                password=self.crypto.encrypt(self.password.get()),
                 portal=portal
             )
             session.add_all([
@@ -62,7 +64,7 @@ class AddPassword:
 
 
 class CredentialList:
-    def __init__(self, tab, root_window, db):
+    def __init__(self, tab, root_window, db, password):
         self.root_window = root_window
         self.tree = ttk.Treeview(
             tab,
@@ -70,6 +72,7 @@ class CredentialList:
             show='headings',
             height=10
         )
+        self.crypto = Crypto(password)
         self.db = db
         self.fill_table_with_data()
         self.config_tree()
@@ -78,9 +81,20 @@ class CredentialList:
     def on_click(self, event):
         item = self.tree.selection()[0]
         selection = self.tree.item(item, 'values')
-        print(selection)
-        self.root_window.clipboard_clear()
-        self.root_window.clipboard_append(selection)
+        with Session(self.db) as session:
+            credential = session.query(Credentials).filter(
+                Portal.name == selection[0],
+                Credentials.login == selection[1]
+            ).one()
+
+            try:
+                decryped = self.crypto.decrypt(credential.password)
+            except InvalidToken:
+                pass
+
+            self.root_window.clipboard_clear()
+            self.root_window.clipboard_append(decryped)
+            print(decryped)
 
     def fill_table_with_data(self):
         with Session(self.db) as session:
@@ -116,27 +130,30 @@ if __name__ == '__main__':
     root.title('Password Manager')
     root.geometry('500x300')
 
+
     def pass_on_click():
-        print(pass_entry.get())
+        tabs.hide(0)
+        credential_list = CredentialList(cred_tab, root, engine, pass_entry.get())
+        AddPassword(add_cred_tab, engine, credential_list, tabs, pass_entry.get())
 
-        pass_label.pack_forget()
-        pass_entry.pack_forget()
-        pass_button.pack_forget()
 
-        tabs = ttk.Notebook(root)
-        cred_tab = tk.Frame(tabs)
-        add_cred_tab = tk.Frame(tabs)
+    #     pass_label.pack_forget()
+    #     pass_entry.pack_forget()
+    #     pass_button.pack_forget()
 
-        tabs.add(cred_tab, text='Credentials')
-        tabs.add(add_cred_tab, text='Add password')
-        tabs.pack(expand=1, fill='both')
+    tabs = ttk.Notebook(root)
+    main_pass_tab = ttk.Frame(tabs)
+    cred_tab = tk.Frame(tabs)
+    add_cred_tab = tk.Frame(tabs)
 
-        credential_list = CredentialList(cred_tab, root, engine)
-        AddPassword(add_cred_tab, engine, credential_list, tabs)
+    tabs.add(main_pass_tab, text='input password')
+    tabs.add(cred_tab, text='Credentials')
+    tabs.add(add_cred_tab, text='Add password')
+    tabs.pack(expand=1, fill='both')
 
-    pass_label = ttk.Label(root, text='Input password:')
-    pass_entry = ttk.Entry(root)
-    pass_button = ttk.Button(root, text='OK', command=pass_on_click)
+    pass_label = ttk.Label(main_pass_tab, text='Input password:')
+    pass_entry = ttk.Entry(main_pass_tab, show='*')
+    pass_button = ttk.Button(main_pass_tab, text='OK', command=pass_on_click)
 
     pass_label.pack(padx=5, pady=5)
     pass_entry.pack(padx=5, pady=5)
